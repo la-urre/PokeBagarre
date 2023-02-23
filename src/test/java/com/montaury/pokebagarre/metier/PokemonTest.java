07c367e751bbe6f4d5f708390e1e2791476891a5
Alexandre Ferreira <ferreira64240@hotmail.com>
2023-02-23 10:32:11 +0100

    Création de PokemonTest.java

---
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
@@ -0,0 +1,12 @@
+package com.montaury.pokebagarre.metier;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PokemonTest {
+@Test
+    void test(){
+
+}
+}
