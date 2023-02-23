84adf482da9a3425e30d5b0f627b5ac92ae822cd
Alexandre Ferreira <ferreira64240@hotmail.com>
2023-02-23 19:53:42 +0100

    Création des 4 premiers test avec les premiers test qui vérifient qui gagne la baguarre avec l'attaque, et les autres tests qui vérifient qui gagne la baguarre avec la défense.

--- src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
@@ -1,22 +1,113 @@
 package com.montaury.pokebagarre.metier;
 
+import com.montaury.pokebagarre.fixtures.ConstructeurDePokemon;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 
 class PokemonTest {
 @Test
     void lePremierEstPlusFort(){
-    //GIVEN
-    Stats stat1 = new Stats(5,6);
-    Stats stat2 = new Stats(4,6);
-    Pokemon poke1 = new Pokemon("Eduardo", "", stat1);
-    Pokemon poke2 = new Pokemon("Romain", "", stat2);
+    // Given
+    Pokemon premierPokemon = ConstructeurDePokemon
+            .unPokemon()
+            .avecAttaque(40)
+            .avecDefense(50)
+            .construire();
 
-    //THEN
-    poke1.estVainqueurContre(poke2);
+    Pokemon deuxiemePokemon = ConstructeurDePokemon
+            .unPokemon()
+            .avecAttaque(30)
+            .avecDefense(40)
+            .construire();
 
-    //THEN
+    // When
+    boolean premierPokemonEstPlusFort = premierPokemon.getStats().getAttaque() > deuxiemePokemon.getStats().getAttaque();
+
+    // Then
+    assertTrue(premierPokemonEstPlusFort);
 }
+
+@Test
+    void leSecondEstPlusFort(){
+        // Given
+        Pokemon premierPokemon = ConstructeurDePokemon
+                .unPokemon()
+                .avecAttaque(40)
+                .avecDefense(50)
+                .construire();
+
+        Pokemon deuxiemePokemon = ConstructeurDePokemon
+                .unPokemon()
+                .avecAttaque(60)
+                .avecDefense(40)
+                .construire();
+
+        // When
+        boolean secondPokemonEstPlusFort = deuxiemePokemon.getStats().getAttaque() > premierPokemon.getStats().getAttaque();
+
+        // Then
+        assertTrue(secondPokemonEstPlusFort);
+    }
+
+    @Test
+    void lePremierAUneMeilleureDefense(){
+
+        //Given
+        Pokemon premierPokemon = ConstructeurDePokemon
+                .unPokemon()
+                .avecAttaque(50)
+                .avecDefense(50)
+                .construire();
+
+        Pokemon deuxiemePokemon = ConstructeurDePokemon
+                .unPokemon()
+                .avecAttaque(50)
+                .avecDefense(40)
+                .construire();
+
+        //When
+        boolean comparaisonAttaque = premierPokemon.getStats().getAttaque() > deuxiemePokemon.getStats().getAttaque();
+        boolean premierPlusFort = premierPokemon.getStats().getDefense() > deuxiemePokemon.getStats().getDefense();
+
+        // Then
+        if (comparaisonAttaque){
+            assertTrue(comparaisonAttaque);}
+        else {
+            assertTrue(premierPlusFort);
+        }
+
+    }
+
+    @Test
+    void leSecondAUneMeilleureDefense(){
+
+        //Given
+        Pokemon premierPokemon = ConstructeurDePokemon
+                .unPokemon()
+                .avecAttaque(50)
+                .avecDefense(50)
+                .construire();
+
+        Pokemon deuxiemePokemon = ConstructeurDePokemon
+                .unPokemon()
+                .avecAttaque(50)
+                .avecDefense(60)
+                .construire();
+
+        //When
+        boolean comparaisonAttaque = deuxiemePokemon.getStats().getAttaque() > premierPokemon.getStats().getAttaque();
+        boolean premierPlusFort = deuxiemePokemon.getStats().getDefense() > premierPokemon.getStats().getDefense();
+
+        // Then
+        if (comparaisonAttaque){
+            assertTrue(comparaisonAttaque);}
+        else {
+            assertTrue(premierPlusFort);
+        }
+
+    }
 }
