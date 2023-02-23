8c9209b068ba0a286c2ae02eddf19b2b9c26523b
Le-Menn Arthur <almenn@iutbayonne.univ-pau.fr>
2023-02-23 10:36:21 +0100

    Ajout de 2 tests supplémentaire sur la class Pokemon

--- src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
@@ -15,12 +15,28 @@ class PokemonTest {
         assertEquals(true, poke1.estVainqueurContre(poke2));
     }
     @Test
+    void moins_attaque_perds(){
+        //GIVEN
+        Pokemon poke1 = new Pokemon("Matis", "dhzhd", new Stats(10, 10));
+        Pokemon poke2 = new Pokemon("Titouan", "dhzhd", new Stats(15, 10));
+        //WHEN & THEN
+        assertEquals(false, poke1.estVainqueurContre(poke2));
+    }
+    @Test
     void meme_attaque_meilleurs_defense_win(){
         //GIVEN
         Pokemon poke1 = new Pokemon("Matis", "dhzhd", new Stats(10, 12));
         Pokemon poke2 = new Pokemon("Titouan", "dhzhd", new Stats(10, 10));
         //WHEN & THEN
         assertEquals(true, poke1.estVainqueurContre(poke2));
+    }
+    @Test
+    void meme_attaque_moins_bonne_defense_perds(){
+        //GIVEN
+        Pokemon poke1 = new Pokemon("Matis", "dhzhd", new Stats(10, 10));
+        Pokemon poke2 = new Pokemon("Titouan", "dhzhd", new Stats(10, 15));
+        //WHEN & THEN
+        assertEquals(false, poke1.estVainqueurContre(poke2));
     }
     @Test
     void meme_stats_premiers_win(){
