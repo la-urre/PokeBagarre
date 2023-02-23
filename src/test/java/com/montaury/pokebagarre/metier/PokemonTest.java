394d2efff9872d91a7a0eec7319b58deed163e4b
Alexandre Ferreira <ferreira64240@hotmail.com>
2023-02-23 10:59:35 +0100

    Ca avance

--- src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
@@ -2,11 +2,21 @@ package com.montaury.pokebagarre.metier;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+
 
 class PokemonTest {
 @Test
-    void test(){
+    void lePremierEstPlusFort(){
+    //GIVEN
+    Stats stat1 = new Stats(5,6);
+    Stats stat2 = new Stats(4,6);
+    Pokemon poke1 = new Pokemon("Eduardo", "", stat1);
+    Pokemon poke2 = new Pokemon("Romain", "", stat2);
 
+    //THEN
+    poke1.estVainqueurContre(poke2);
+
+    //THEN
 }
 }
