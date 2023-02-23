5d7d8758bbe7615ad0b9a666c88b504025ae3e5a
RomainG <rgouaud@iutbayonne.univ-pau.fr>
2023-02-23 10:51:41 +0100

    creation du squelette des tests

---
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
@@ -0,0 +1,56 @@
+package com.montaury.pokebagarre.metier;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PokemonTest {
+
+
+
+    @Test
+    void should_the_first_to_be_the_strongest_and_win(){
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+    }
+
+    @Test
+    void should_the_second_to_be_the_strongest_and_win(){
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+    }
+
+    @Test
+    void should_the_first_to_have_the_higher_defense_and_win(){
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+    }
+
+    @Test
+    void should_the_second_to_have_the_higher_defense_and_win(){
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+    }
+
+    @Test
+    void should_both_have_equals_stats_so_the_first_win(){
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+    }
+
+}
